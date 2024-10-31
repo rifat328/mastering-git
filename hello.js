f23fccc2b00ec7d03ef7723e74eb9b0ac46f0343
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-10-31 22:08:28 +0600

    Reverting code 3

--- hello.js
+++ hello.js
@@ -9,3 +9,6 @@ console.log("Hello from def-Rifat");
 console.log("hello git");
 console.log("hello World");
 console.log("hello World3c");
+
+console.log("hello World3csafddadsfasdfasdf");
+console.log("This is a bad code bug bug bug");
