0f7e79bd1783f3b748874d5bf13c9c230c8dfaff
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-10-31 22:36:50 +0600

    aksdlkjsdfajlk

--- hello.js
+++ hello.js
@@ -8,14 +8,3 @@ console.log("Hello from def-Rifat");
 //Trying git revert ..............
 console.log("hello git");
 console.log("hello World");
-<<<<<<< HEAD
-console.log("hello World3c");
-
-console.log("hello World3csafddadsfasdfasdf");
-console.log("This is a bad code bug bug bug");
-
-console.log("Hello from def-Rifat");
-console.log("Hello from def-Rifat");
-=======
-console.log("hello World3c");
->>>>>>> parent of f23fccc ( Reverting code 3)
