d2b4edb02f5ec382516bb7fbca42f7fe77ddb427
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-10-31 22:31:41 +0600

    Revert " Reverting code 3"
    This reverts commit f23fccc2b00ec7d03ef7723e74eb9b0ac46f0343.

--- hello.js
+++ hello.js
@@ -8,6 +8,7 @@ console.log("Hello from def-Rifat");
 //Trying git revert ..............
 console.log("hello git");
 console.log("hello World");
+<<<<<<< HEAD
 console.log("hello World3c");
 
 console.log("hello World3csafddadsfasdfasdf");
@@ -15,3 +16,6 @@ console.log("This is a bad code bug bug bug");
 
 console.log("Hello from def-Rifat");
 console.log("Hello from def-Rifat");
+=======
+console.log("hello World3c");
+>>>>>>> parent of f23fccc ( Reverting code 3)
