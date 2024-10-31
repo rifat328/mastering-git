53526db54d9b37d945999b45d9448ae0e6273c87
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-10-31 21:35:54 +0600

    console.log def- Rifat

--- hello.js
+++ hello.js
@@ -3,3 +3,5 @@ console.log("hello World");
 console.log("hello World3c");
 console.log("hello World3csafddadsfasdfasdf");
 console.log("This is a bad code bug bug bug");
+console.log("Hello from def-Rifat");
+console.log("Hello from def-Rifat");
